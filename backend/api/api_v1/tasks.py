f37a93fa17785554db56796672c0610af04ff6b5
Maks <fray09@inbox.ru>
2024-11-04 00:39:41 +0600

    add task update route

--- backend/api/api_v1/tasks.py
+++ backend/api/api_v1/tasks.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper, Task
-from core.schemas.tasks import TaskRead, TaskCreate
+from core.schemas.tasks import TaskRead, TaskCreate, TaskUpdate
 
 from crud import tasks as tasks_crud
 
@@ -13,13 +13,14 @@ router = APIRouter(
     tags=["Tasks"],
 )
 
+
 @router.post("", response_model=TaskRead)
-async def create_user(
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter)
-    ],
-    task_create: TaskCreate,
+async def create_task(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter)
+        ],
+        task_create: TaskCreate,
 ) -> Task:
     task = await tasks_crud.create_task(
         session=session,
@@ -28,11 +29,11 @@ async def create_user(
 
 
 @router.get("", response_model=list[TaskRead])
-async def get_all_task(
-session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter)
-    ],
+async def get_all_tasks(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter)
+        ],
 ):
     tasks = await tasks_crud.get_all_tasks(session=session)
     return tasks
@@ -52,6 +53,18 @@ async def get_task(
             status_code=404,
             detail=f"Todo with id {task_id} not found"
         )
-
-
-
+@router.put("/{task_id}", response_model=TaskRead)
+async def patch_task_by_id(
+        task_update: TaskUpdate,
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter)
+        ],
+        task: Task = Depends(tasks_crud.task_by_id),
+):
+    updated_task = await tasks_crud.update_task(
+        session=session,
+        task=task,
+        task_update=task_update
+    )
+    return updated_task
